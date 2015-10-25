2205f393c8dd17691b97bdde152c91b7a3f8840a
David Turner <cillian64@googlemail.com>
2015-10-25 12:36:45 +0000

    Generalised menu structure

--- firmware/menu.c
+++ firmware/menu.c
@@ -2,74 +2,99 @@
 #include <stdio.h>
 #include "menu.h"
 #include "ch.h"
-#include "chprintf.h"
 #include "drivers/oled.h"
 #include "drivers/rotenc.h"
-#include "rtc.h"
 #include "clock.h"
 
-static event_listener_t rotenc_el;
-
 typedef struct {
-    const char name[16];
+    const char big_text[16];
+    const char small_text[16];
     const uint8_t* icon;
+    void (*func)(void);
 } menu_item;
 
-#define MENU_LEN (11)
-static const menu_item menu_items[MENU_LEN] = {
-    {"Beef", (const uint8_t*)oled_icon_cow},
-    {"Pork", (const uint8_t*)oled_icon_pig},
-    {"Lamb", (const uint8_t*)oled_icon_sheep},
-    {"Duck", (const uint8_t*)oled_icon_duck},
-    {"Fish", (const uint8_t*)oled_icon_fish},
-    {"Eggs", (const uint8_t*)oled_icon_eggs},
-    {"Custom", (const uint8_t*)oled_icon_thermo},
-    {"Display Time", NULL},
-    {"Set Time", NULL},
-    {"Set backup reg", NULL},
-    {"Read backup reg", NULL}
+void do_menu(const menu_item *menu_items, uint8_t num_items);
+void do_beef(void);
+
+static const uint8_t root_menu_len = 11;
+static const menu_item root_menu[11] = {
+    {"COOK:", "Beef", (const uint8_t*)oled_icon_cow, do_beef},
+    {"COOK:", "Pork", (const uint8_t*)oled_icon_pig, NULL},
+    {"COOK:", "Lamb", (const uint8_t*)oled_icon_sheep, NULL},
+    {"COOK:", "Duck", (const uint8_t*)oled_icon_duck, NULL},
+    {"COOK:", "Fish", (const uint8_t*)oled_icon_fish, NULL},
+    {"COOK:", "Eggs", (const uint8_t*)oled_icon_eggs, NULL},
+    {"COOK:", "Custom", (const uint8_t*)oled_icon_thermo, NULL},
+    {"SETTINGS:", "Display Time", NULL, rtc_disp_time},
+    {"SETTINGS:", "Set Time", NULL, rtc_set_time},
+    {"SETTINGS:", "Set backup reg", NULL, rtc_set_bkup},
+    {"SETTINGS:", "Read backup reg", NULL, rtc_disp_bkup}
 };
+
+static const uint8_t beef_menu_len = 6;
+static const menu_item beef_menu[6] = {
+    {"BEEF:", "Rare", (const uint8_t*)oled_icon_cow, NULL},
+    {"BEEF:", "Med Rare", (const uint8_t*)oled_icon_cow, NULL},
+    {"BEEF:", "Medium", (const uint8_t*)oled_icon_cow, NULL},
+    {"BEEF:", "Med Done", (const uint8_t*)oled_icon_cow, NULL},
+    {"BEEF:", "Well Done", (const uint8_t*)oled_icon_cow, NULL},
+    {"", "Back", NULL, NULL},
+};
+
+void do_menu(const menu_item *menu_items, uint8_t num_items)
+{
+    int menu_idx = 0;
+    eventflags_t eflags;
+    event_listener_t rotenc_el;
+    chEvtRegister(&rotenc_es, &rotenc_el, 0);
+    while(1)
+    {
+        oled_erase();
+        oled_text_big(0, 0, menu_items[menu_idx].big_text);
+        oled_text_small(3, 0, menu_items[menu_idx].small_text);
+        oled_icon(menu_items[menu_idx].icon);
+        oled_draw();
+
+        chEvtWaitOne(ALL_EVENTS);
+        eflags = chEvtGetAndClearFlags(&rotenc_el);
+        if(eflags & ROTENC_LEFT_FLAG)
+        {
+            menu_idx -= 1;
+            if(menu_idx < 0)
+                menu_idx = num_items - 1;
+        }
+        else if(eflags & ROTENC_RIGHT_FLAG)
+        {
+            menu_idx += 1;
+            if(menu_idx >= num_items)
+                menu_idx = 0;
+        }
+        else if(eflags & ROTENC_PRESS_FLAG)
+        {
+            rotenc_led(ROTENC_CYAN);
+            chThdSleepMilliseconds(200);
+            rotenc_led(ROTENC_BLANK);
+            /* Unregister before going back or calling func */
+            chEvtUnregister(&rotenc_es, &rotenc_el);
+            /* NULL function acts as back button */
+            if(menu_items[menu_idx].func == NULL)
+                return;
+            else
+                (*(menu_items[menu_idx].func))();
+            chEvtRegister(&rotenc_es, &rotenc_el, 0);
+        }
+    }
+}
+
+void do_beef(void)
+{
+    do_menu(beef_menu, beef_menu_len);
+}
 
 void MenuThread(void* arg)
 {
     (void)arg;
 
-    int menu_idx = 0;
-    eventflags_t eflags;
-
-    chEvtRegister(&rotenc_es, &rotenc_el, 0);
-
-    while(true) {
-        oled_erase();
-        oled_text_big(0, 0, "COOK:");
-        oled_icon(menu_items[menu_idx].icon);
-        oled_text_small(3, 0, menu_items[menu_idx].name);
-        oled_draw();
-
-        chEvtWaitOne(ALL_EVENTS);
-        eflags = chEvtGetAndClearFlags(&rotenc_el);
-        if(eflags & ROTENC_LEFT_FLAG) {
-            menu_idx -= 1;
-            if(menu_idx < 0)
-                menu_idx = MENU_LEN - 1;
-        } else if(eflags & ROTENC_RIGHT_FLAG) {
-            menu_idx += 1;
-            if(menu_idx >= MENU_LEN)
-                menu_idx = 0;
-        } else if(eflags & ROTENC_PRESS_FLAG) {
-            oled_erase();
-            oled_draw();
-            rotenc_led(ROTENC_WHITE);
-            chThdSleepMilliseconds(200);
-            rotenc_led(ROTENC_BLANK);
-            if(menu_idx == 7)
-                rtc_disp_time();
-            if(menu_idx == 8)
-                rtc_set_time();
-            if(menu_idx == 9)
-                rtc_set_bkup();
-            if(menu_idx == 10)
-                rtc_disp_bkup();
-        }
-    }
+    while(1)
+        do_menu(root_menu, root_menu_len);
 }
